e1d2106862cead44095f207a431ff10ddf119fd9
Wissal <wissalaitihya@gmail.com>
2026-03-06 12:34:49 +0000

    how many numbers are even

--- challenge5.4.php
+++ challenge5.4.php
@@ -1,11 +1,12 @@
 <?php
-$count =1;
-$total= 0;
-while ($count <= 50){
-   if($count %2 ==0){
-    $total++;
-   }
-   $count++;
+$count=1;
+$total=0;
+
+while($count <= 50){
+    if ($count % 2==0){
+        $total++;
+    }
+    $count++;
 }
-echo "total even number " . $total . "<br>";
+echo "total even number: " .$total ;
 ?>
