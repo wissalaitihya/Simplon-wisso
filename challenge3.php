a71e795a7b1dbafc445879b695d6129159372027
Wissal <wissalaitihya@gmail.com>
2026-03-06 12:28:23 +0000

    modification

--- challenge3.php
+++ challenge3.php
@@ -1,18 +1,17 @@
 <?php
 $friends = [
- "Doha" => 70,
+ "Doha" => 70, //name => money 
  "Hiba" => 150,
  "Meryem" => 200,
  "Ikhlass" => 30,
  "Salma" => 90
 ];
-$total = 0;
-foreach($friends as $name => $money) {
-    $total = $total + $money;
+$total = 0; //let the count begin from 0
+foreach($friends as $name => $money) { //take each $name=>$money from the array 
+    $total =$total + $money; 
     if($money > 100 ) {
        echo $name . " " . "owes more than 100DH:" ." " . $money . "DH" . "<br>";
+    }
 }
-}
-
-
+ echo "the total is:" . " " . $total . "DH" . "<br>";
 ?>
