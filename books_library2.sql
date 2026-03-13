9d46e18f07547cb0912459aebff14d7d1a7f6bea
Wissal <wissalaitihya@gmail.com>
2026-03-13 11:23:21 +0000

    challenge 2 sql

---
+++ books_library2.sql
@@ -0,0 +1,10 @@
+INSERT INTO library_books
+VALUES (1,"book A","Author 1","1999","Available","25.00"),
+       (2,"book B","Author 2","2020","Available","11.99"),
+       (3,"book C","Author 3","2025","Borrowed","18.50"),
+	   (4,"book D","Author 2","2023","Available","19.99"),
+       (5,"book E","Author 4","1950","Available","39.00"),
+       (6,"book A","Author 5","2012","Borrowed","25.00"),
+       (7,"book A","Author 6","2017","Lost", NULL),
+       (8,"book A","Author 7","2022","Available","25.00");
+SELECT * FROM library_books;
