ff5ae036f3575a557b91014c344255795a0cfe84
Wissal <wissalaitihya@gmail.com>
2026-03-13 12:26:03 +0000

    sql2

--- books_library2.sql
+++ books_library2.sql
@@ -1,10 +1,10 @@
-INSERT INTO library_books
-VALUES (1,"book A","Author 1","1999","Available","25.00"),
-       (2,"book B","Author 2","2020","Available","11.99"),
-       (3,"book C","Author 3","2025","Borrowed","18.50"),
-	   (4,"book D","Author 2","2023","Available","19.99"),
-       (5,"book E","Author 4","1950","Available","39.00"),
-       (6,"book A","Author 5","2012","Borrowed","25.00"),
-       (7,"book A","Author 6","2017","Lost", NULL),
-       (8,"book A","Author 7","2022","Available","25.00");
+INSERT INTO library_books (title, author, published_year, status, price)
+VALUES ("Pride and Prejudice","Jane Austen","1999","Available","250"),
+       ("1984","George Orwell","2020","Available","120"),
+       ("Donated Book","Unknown Author","2025","Borrowed","185"),
+	   ("Animal Farm","George Orwell","2023","Available","200"),
+       ("Strangers on a Train","Patricia Highsmith","1950","Available","300"),
+       ("The Hobbit","The Hobbit","2012","Borrowed","250"),
+       ("To Kill a Mockingbird","Harper Lee","2017","Lost", NULL),
+       ("The Great Gatsby","F. Scott Fitzgerald","2022","Available","100");
 SELECT * FROM library_books;
