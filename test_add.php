8c57e461b146e4352aa844c05e6271b2141d3b4b
Wissal <wissalaitihya@gmail.com>
2026-03-15 03:05:32 +0000

    sql9

---
+++ test_add.php
@@ -0,0 +1,19 @@
+<?php
+require "db.php";   
+
+$title = "Learning PHP";
+$author = "John Smith";
+$price = 180;
+
+$sql = "INSERT INTO library_books (title, author, price)
+        VALUES (:title, :author, :price)";
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute([
+    'title' => $title,
+    'author' => $author,
+    'price' => $price
+]);
+$id = $pdo->lastInsertId();
+echo "Success! Book added with ID: " . $id;
+?>
